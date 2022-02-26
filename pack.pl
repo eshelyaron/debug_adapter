79b2934f4fb5e676dc72b5b012e5f1f6f6ef805d
Eshel Yaron <eshel@areionsec.com>
2022-02-26 13:04:38 +0200

    Bump patch version to 0.4.6

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
 name(debug_adapter).
 title('Debug Adapter Protocol package for SWI-Prolog').
-version('0.4.5').
+version('0.4.6').
 author('Eshel Yaron', 'eshelshay.yaron@gmail.com').
 maintainer('Eshel Yaron', 'eshelshay.yaron@gmail.com').
 packager('Eshel Yaron', 'eshelshay.yaron@gmail.com').
 home('https://github.com/eshelyaron/debug_adapter').
-download('https://github.com/eshelyaron/debug_adapter/releases/download/v0.4.5/debug_adapter-0.4.5.zip').
+download('https://github.com/eshelyaron/debug_adapter/releases/download/v0.4.6/debug_adapter-0.4.6.zip').
