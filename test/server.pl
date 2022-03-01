8fd85243d6862d292df74e6ab75121812fa261a0
Eshel Yaron <eshel@areionsec.com>
2022-03-01 23:35:24 +0200

    TEST: Add SWI-Prolog 8.2.4 target to CI

--- test/server.pl
+++ test/server.pl
@@ -17,8 +17,9 @@ dapipe(ServerIn, ServerOut, ClientIn, ClientOut) :-
 
 :- begin_tests(server).
 
-:- use_module("../prolog/debug_adapter/client.pl").
 :- use_module("../prolog/debug_adapter/server.pl").
+:- use_module("../prolog/debug_adapter/client.pl").
+
 
 test(disconnect, [ setup(dapipe(ServerIn, ServerOut, ClientIn, ClientOut)),
                    cleanup(( close(ServerIn),
@@ -282,20 +283,20 @@ test(stepInTargets, [ setup(dapipe(SIn, SOut, CIn, COut)),
     ;   dap_await_event(CIn, "stopped", StoppedBody0)
     ),
     StoppedBody0 = _{description:_, hitBreakpointIds:_, reason:"entry", text:_, threadId:Id},
-    dap_request_response(CIn, COut, 5, "stackTrace", _{threadId: Id}, Events0, Body0),
+    dap_request_response(CIn, COut, 5, "stackTrace", _{threadId: Id}, _, Body0),
     get_dict(stackFrames, Body0, [ActiveFrame|_]),
     get_dict(id, ActiveFrame, FrameId),
-    dap_request_response(CIn, COut, 6, "stepInTargets", _{frameId: FrameId}, Events1, Body1),
+    dap_request_response(CIn, COut, 6, "stepInTargets", _{frameId: FrameId}, _, Body1),
     assertion(Body1 = _{targets:[_{id:0, label:"step"}, _{id:1, label:"fail"}]}),
     dap_request_response(CIn, COut, 7, "stepIn", _{threadId: Id}),
     dap_request_response(CIn, COut, 8, "stepIn", _{threadId: Id}),
     dap_request_response(CIn, COut, 9, "stepIn", _{threadId: Id}),
     dap_request_response(CIn, COut, 10, "stepIn", _{threadId: Id}),
     dap_request_response(CIn, COut, 11, "stepIn", _{threadId: Id}),
-    dap_request_response(CIn, COut, 12, "stackTrace", _{threadId: Id}, Events2, Body2),
+    dap_request_response(CIn, COut, 12, "stackTrace", _{threadId: Id}, _, Body2),
     get_dict(stackFrames, Body2, [ActiveFrame2|_]),
     get_dict(id, ActiveFrame2, FrameId2),
-    dap_request_response(CIn, COut, 13, "stepInTargets", _{frameId: FrameId2}, Events3, Body3),
+    dap_request_response(CIn, COut, 13, "stepInTargets", _{frameId: FrameId2}, _, Body3),
     assertion(Body3 = _{targets:[_{id:0, label:"step"}]}),
     dap_request_response(CIn, COut, 14, "disconnect"),
     thread_join(ServerThreadId, E),
