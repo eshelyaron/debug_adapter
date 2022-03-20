0dae26070e2e35001b835911834821c0f1d6ffa3
Eshel Yaron <eshel@areionsec.com>
2022-03-20 11:15:31 +0200

    REFACTOR: Document new `da_server/1` modular interface

--- prolog/debug_adapter/sdk.pl
+++ prolog/debug_adapter/sdk.pl
@@ -1,14 +1,25 @@
 :- module(
        da_sdk,
        [
-           da_sdk_response/3,
-           da_sdk_response/4,
-           da_sdk_error/4,
-           da_sdk_event/2,
-           da_sdk_event/3,
-           da_sdk_stop/1
+           da_sdk_response/3,  % da_sdk_response(+Handle, +RequestSeq, +Command)
+           da_sdk_response/4,  % da_sdk_response(+Handle, +RequestSeq, +Command, +Body)
+           da_sdk_error/4,     % da_sdk_response(+Handle, +RequestSeq, +Command, +Message)
+           da_sdk_event/2,     % da_sdk_response(+Handle, +Type)
+           da_sdk_event/3,     % da_sdk_response(+Handle, +Type, +Body)
+           da_sdk_stop/1       % da_sdk_response(+Handle)
        ]
    ).
+
+
+/** <module> SWI-Prolog Debug Adapter Protocol SDK
+
+Facilities for implementing DAP servers. The predicates in this module
+are designed to be called during a DAP session to modify the state of
+the session or to communicate with the DAP client.
+
+@see Option =on_command= of =da_server/1=.
+*/
+
 
 da_sdk_action(Handle, Action) :-
     thread_send_message(Handle, action(Action)).
